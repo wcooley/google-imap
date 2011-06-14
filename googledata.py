8cfa7e2642c4d3468761de119d479a90b8659386
Chris Chen <chchen@pdx.edu>
2011-06-14 15:30:57 -0700

    Added "limit" to allusernames.

--- googledata.py
+++ googledata.py
@@ -28,15 +28,27 @@ class domaininfo:
         return usernames
 
 
-    def allusernames(self):
-        """Builds a nested list of all the usernames in the Google apps domain."""
+    def allusernames(self, limit=None):
+        """Builds a nested list of all the usernames in the Google apps domain. If limit is defined, the fetching will stop when the number of users collected meets or exceeds it."""
         userlists = list()  # The nested list
 
         userlist = self._nexthundred(start_at_beginning=True)
         userlists.append(userlist)
 
-        while len(userlist) > 1:    # Unless the list is empty or a singleton
-            userlist = self._nexthundred(start_at_beginning=False)
-            userlists.append(userlist)
+        if limit == None:
+            while len(userlist) > 1:    # Unless the list is empty or a singleton
+                userlist = self._nexthundred(start_at_beginning=False)
+                userlists.append(userlist)
+
+        else:
+            if limit < 0:
+                raise Exception("Limit must be None or a positive integer")
+
+            count = 0
+
+            while (len(userlist) > 1) and (count < limit):
+                userlist = self._nexthundred(start_at_beginning=False)
+                userlists.append(userlist)
+                count += len(userlist)
 
         return userlists
