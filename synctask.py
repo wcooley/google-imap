7da66f268bcff63c27f6483952ad11b3f7e28368
Chris Chen <chchen@pdx.edu>
2011-06-17 16:31:35 -0700

    Added celery task modules

---
+++ synctask.py
@@ -0,0 +1,117 @@
+from celery.task import task
+from os import uname
+from time import sleep, time
+from psuldap import psuldap
+import shlex, subprocess
+import memcache
+
+@task(ignore_result=True)
+def imapsync(ldapuri=None, memcaches=None, imapserver=None, adminuser=None, plevel="test", dryrun=True, runlimit=1800, user=None):
+    imapsync_dir = "/opt/google-imap/"
+    imapsync_cmd = imapsync_dir + "imapsync"
+    cyrus_pf = imapsync_dir + "cyrus.pf"
+    extra_opts = "--delete2"
+
+    if dryrun:
+        extra_opts = extra_opts + " --dry" 
+
+    if runlimit <= 0:
+        raise Exception("Runlimit must be a positive integer.")
+
+    if plevel == "prod":
+        google_pf = imapsync_dir + "google-prod.pf"
+        google_domain = "pdx.edu"
+
+    elif plevel == "test":
+        google_pf = imapsync_dir + "google-test.pf"
+        google_domain = "gtest.pdx.edu"
+
+    else:
+        raise Exception("Plevel must be test or prod.")
+
+    command = imapsync_cmd + " --pidfile /tmp/imapsync-" + user + ".pid --host1 " + imapserver + " --port1 993 --user1 " + user + " --authuser1 " + adminuser + " --passfile1 " + cyrus_pf + " --host2 imap.gmail.com --port2 993 --user2 " + user + "@" + google_domain + " --passfile2 " + google_pf + " --ssl1 --ssl2 --maxsize 26214400 --authmech1 PLAIN --authmech2 XOAUTH -sep1 '/' --exclude '^Shared Folders' " + extra_opts
+    directory = psuldap()   # Our LDAP handle
+    directory.connect(ldapuri)  # Anonymous bind
+
+    # Search for the user's mailHost attribute.
+    mailhostsearch = directory.search(searchfilter="(uid=%s)" % user, attrlist=["mailHost"]) 
+
+    # Loop through the search results. If any them match gmx.pdx.edu, abort.
+    for (dn, result) in mailhostsearch:
+        if result.has_key("mailHost"):
+            if result["mailHost"] == "gmx.pdx.edu":
+                raise Exception("User %s already migrated to Google." % user)
+
+    cache = memcache.Client(servers=memcaches)  # Our memcache handle
+    cachekey = "(%s,auto)" % user
+
+    cachestate = cache.gets(cachekey)
+
+    if cachestate == None: # Maybe the cache has been cleared. Continue.
+        pass
+
+    # Well, it looks like the cache has another task's data for this user. Abort.
+    elif cachestate["status"] != "queued" or cachestate["taskid"] != imapsync.request.id:
+        raise Exception("Cache inconsistency error for user %s." % user)
+
+    # We're good to go. Let's set the cache with our new state.
+    runstate = {
+        "status":"running"
+        ,"timestamp":int(time())
+        ,"taskid":imapsync.request.id
+        ,"worker":uname()[1]
+    }
+
+    cachelimit = runlimit + 10  # Fudge factor. 5 seconds for the sleep, 5 seconds for fudge.
+    
+    if cache.cas(cachekey, runstate, time=cachelimit) != True: # Whoops, something changed. Abort.
+        raise Exception("Cache inconsistency error for user %s." % user)
+
+    syncprocess = subprocess.Popen(
+        shlex.split(command)
+        ,stdout=subprocess.PIPE
+        ,stderr=subprocess.PIPE
+    )
+
+    starttime = time()
+
+    # While the process is running, and we're under the time limit
+    while (syncprocess.poll() == None) and ((time() - starttime) < runlimit):
+        sleep(5)
+    
+    # Are we still running? Send a SIGTERM to the process and throw the task back on the queue.
+    # This is done to prevent one user from tying up a worker for longer than the runlimit.
+    if (syncprocess.poll() == None):
+        syncprocess.terminate()     # Send SIGTERM
+        syncprocess.communicate()   # Read stdin/out, and wait for process to terminate
+        exitstatus = "timeout"
+
+    else:
+        if syncprocess.returncode == 0:
+            exitstatus = "ok"
+        else:
+            exitstatus = "error_%d" % syncprocess.returncode
+
+    cachestate = cache.gets(cachekey)
+
+    if cachestate == None: # Maybe the cache has been cleared. Continue.
+        pass
+
+    # Check to see if our cache state matches the previously set state. 
+    if cachestate != runstate:
+        raise Exception("Cache inconsistency error for user %s." % user)
+
+    # We're good to go. Let's set the cache with our new state.
+    endstate = {
+        "status":"complete"
+        ,"timestamp":int(time())
+        ,"taskid":imapsync.request.id
+        ,"worker":uname()[1]
+        ,"returned":exitstatus
+        ,"runtime":int(time() - starttime)
+    }
+    
+    if cache.cas(cachekey, endstate) != True: # Whoops, something changed. Abort.
+        raise Exception("Cache inconsistency error for user %s." % user)
+
+    return (user, endstate)
---
+++ syncutils.py
@@ -0,0 +1,119 @@
+from synctask import imapsync
+from psuldap import psuldap
+from googledata import domaininfo
+from getpass import getpass
+from time import sleep, time
+import memcache
+
+class usersync:
+    def __init__(self, plevel="test", dryrun=True, runlimit=600, ldapuri=None, memcaches=None, imapserver=None, adminuser=None):
+        """Initializes a usersync object. Plevel is either test or prod, dryrun is a boolean and runlimit is an integer. Uses memcaches, provided as a list, [host:port,...], for storing task state data. Imapserver and adminuser are for the local (non-Google side). Ldapuri is a uri for our LDAP directory."""
+        self.plevel = plevel
+        self.dryrun = dryrun
+        self.runlimit = runlimit
+        self.memcaches = memcaches
+        self.imapserver = imapserver
+        self.adminuser = adminuser
+        self.ldapuri = ldapuri
+
+
+    def populate(self):
+        """Connects to Google domain, populates a list of usernames, and filters out against a static list of opt-outs and against the ldap directory. Creates a nested list of usernames."""
+        optouts = [ "janely", "leschins", "cfrl", "jensenmm", "polly", "nelsonk", "kerrigs", "pats", "wamserc", "wacke", "smithcc", "psu25042", "mackc", "powells", "mjantzen", "pcooper", "staplej", "pmueller", "ferguse" ]
+    
+        if self.plevel == "prod":
+            gdomain = "pdx.edu"
+
+        elif self.plevel == "test":
+            gdomain = "gtest.pdx.edu"
+
+        elif self.plevel == "devl":
+            gdomain = "gdev.pdx.edu"
+
+        else:
+            raise Exception("Invalid plevel %s" % self.plevel)
+
+        guser = raw_input("Username: ")
+        gpass = getpass()
+    
+        google = domaininfo(user=guser, password=gpass, domain=gdomain)
+    
+        print("Gathering all usernames for Google apps domain %s" % gdomain)
+        googleuserlists = google.allusernames()
+    
+        optinuserlists = list()
+    
+        print("Screening out early migration opt-opt users")
+        for userlist in googleuserlists:
+            optinuserlists.append([user for user in userlist if user not in optouts])
+    
+        directory = psuldap()
+        directory.connect(ldapurl="ldap://ldap1.oit.pdx.edu")
+
+        self.userlists = list()
+    
+        print("Screening out non-LDAP users")
+        for userlist in optinuserlists:
+            self.userlists.append([user for user in userlist if directory.exists("(uid=%s)" % user)])
+
+        print("Ready to launch!")
+
+
+    def launchuser(self, user=None):
+        """Submits a asynchronous task for a given user, first checking memcache to see if there are extent tasks--if there are, it returns None. If clear, it returns the task id of the queued task."""
+        cache = memcache.Client(servers=self.memcaches)
+        cachekey = "(%s,auto)" % user
+
+        try:    # If we can't contact the cache, we're in trouble.
+            userstate = cache.gets(cachekey)
+
+        except:
+            return None
+
+        if userstate == None or userstate["status"] == "complete": # No task or complete, ready to go.
+            try:
+                task = imapsync.delay(
+                    ldapuri=self.ldapuri
+                    ,plevel=self.plevel
+                    ,dryrun=self.dryrun
+                    ,runlimit=self.runlimit
+                    ,memcaches=self.memcaches
+                    ,imapserver=self.imapserver
+                    ,adminuser=self.adminuser
+                    ,user=user
+                )
+
+            except: # Problem launching the process? Return False.
+                return None
+
+            cachedata = {"status":"queued", "timestamp":int(time()), "taskid":task.task_id}
+
+            if cache.cas(cachekey, cachedata) == True:   # Return the task_id
+                return task.task_id
+
+            else: # We had some trouble with the cache. Revoke the process and return None.
+                task.revoke()   # If this throws an exception, we have problems.
+
+        else:
+            return None
+
+
+    def launchgroup(self, interval=0.5):
+        """Launches the tasksets created using populate(). Interval is the time between submissions."""
+        self.jobs = list()
+
+        for userlist in self.userlists:
+            for username in userlist:
+                taskid = self.launchuser(user=username)
+
+                if taskid == None:
+                    print("Submission NOT OK for user: %s" % username)
+
+                else:
+                    print("Submission OK for user: %s. Task ID: %s" % (username, taskid))
+
+                self.jobs.append((username,taskid))
+
+                sleep(interval)
+
+        print("Done!")
